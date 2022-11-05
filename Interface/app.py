cb891f53394a94d42777f04a43425e1a1d4a9280
Naftal Rainer <nrainer001@gmail.com>
2022-11-05 14:45:32 +0300

    SetUp: Template Inheritance

--- Interface/app.py
+++ Interface/app.py
@@ -2,7 +2,7 @@ from flask import  Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-# @app.route('/')
+# @app.route('/home')
 # def home():
 #     return render_template('index.html')
 
