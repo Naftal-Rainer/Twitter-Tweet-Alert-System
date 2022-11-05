739ac012841852950ad0df54942c3d85e6e32f15
Naftal Rainer <nrainer001@gmail.com>
2022-11-05 22:05:26 +0300

    Javascript validations

--- Interface/static/script.js
+++ Interface/static/script.js
@@ -1,35 +1,70 @@
 // Sign-up validations
 
-function validateForm(){
-    let usern = document.forms["myForm"]["username"].value;
-    let eml = document.forms["myForm"]["email"].value;
-    let passw = document.forms["myForm"]["pswd"].value;
-    let cpassw = document.forms["myForm"]["cnpswd"].value;
-    let rem = document.forms["myForm"]["remember"].value;
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirm_password = document.getElementById('confirm_password');
 
-    if (usern == "") {
-        alert("Name must be filled out");
-        return false;
+form.addEventListener('submit', e =>{
+    e.preventDefault();
+
+    validateInputs();
+});
+
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const confirm_passwordValue = confirm_password.value.trim();
+
+    if (usernameValue === null || usernameValue === "") {
+        setError(username, 'Username is required');
+    alert('Wueh')
+
+    } else {
+        setSuccess(username);
     }
 
-    else if(eml == ""){
-        alert("Email must be filled out");
-        return false;
+    if (emailValue === null || emailValue === "") {
+        setError(email, 'Email is required');
+    } else {
+        setSuccess(email);
     }
 
-    else if(passw == ""){
-        alert("Password must be filled out");
-        return false;
+    if (passwordValue === null || passwordValue === "") {
+        setError(email, 'Password is required');
+    } else if(passwordValue < 8){
+        setError(passwordValue, 'Password must be at least 8 character');
+    } else {
+        setSuccess(passwordValue);
     }
 
-    else if(cpassw = ""){
-        alert("Confirm your password");
-        return false;
+    if (confirm_passwordValue === null || confirm_passwordValue === "") {
+        setError(email, 'Please confirm your password');
+    } else if(confirm_passwordValue != passwordValue){
+        setError(confirm_passwordValue, 'Passwords doesn\'t match');
+    } else {
+        setSuccess(confirm_passwordValue);
     }
+};
 
-    else if(passw != cpassw){
-        alert("Does not match the password");
-        return false;
-    }
-    
-}
